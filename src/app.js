74baba695f5cabd320774afcd2b065091f21e89d
Skyler Kidd <f.s.kidd@gmail.com>
2015-06-27 13:49:37 -0700

    Click Up call

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@
 
 //import statements
 var UI = require('ui');
-var ajax = require('ajax');
 
 //create main app
 var main = new UI.Card({
@@ -19,20 +18,25 @@ var main = new UI.Card({
 //show the app
 main.show();
 
+//on click
+main.on('click', 'up', function(e) {
+  client.calls.create({ 
+  to: "4258762036", 
+  from: "+12019321119",   
+  method: "GET",  
+  fallbackMethod: "GET",  
+  statusCallbackMethod: "GET",    
+	record: "false" 
+  }, function(err, call) { 
+	console.log(call.sid); 
+});
+});
+
 // Twilio Credentials 
 var accountSid = 'ACd81a88c058941f561b0d3506916bd76f'; 
 var authToken = 'd490efd31d0f629ee0fd0cf430b31607'; 
  
 //require the Twilio module and create a REST client 
-var client = require('twilio')(accountSid, authToken); 
+var client = require('twilio')(accountSid, authToken);
  
-client.calls.create({ 
-	to: "4258762036", 
-	from: "+12019321119",   
-	method: "GET",  
-	fallbackMethod: "GET",  
-	statusCallbackMethod: "GET",    
-	record: "false" 
-}, function(err, call) { 
-	console.log(call.sid); 
-});
+
