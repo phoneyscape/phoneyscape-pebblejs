5ed8a059c128566d061c5e2c064620b9c158967c
SkylerKidd <f.s.kidd@gmail.com>
2015-06-27 14:32:11 -0700

    Remove twilio module attempt

--- src/js/app.js
+++ src/js/app.js
@@ -20,23 +20,9 @@ main.show();
 
 //on click
 main.on('click', 'up', function(e) {
-  client.calls.create({ 
-  to: "4258762036", 
-  from: "+12019321119",   
-  method: "GET",  
-  fallbackMethod: "GET",  
-  statusCallbackMethod: "GET",    
-	record: "false" 
-  }, function(err, call) { 
-	console.log(call.sid); 
-});
+
 });
 
-// Twilio Credentials 
-var accountSid = 'ACd81a88c058941f561b0d3506916bd76f'; 
-var authToken = 'd490efd31d0f629ee0fd0cf430b31607'; 
- 
-//require the Twilio module and create a REST client 
-var client = require('twilio')(accountSid, authToken);
- 
-
+// Twilio Credentials
+var accountSid = 'ACd81a88c058941f561b0d3506916bd76f';
+var authToken = 'd490efd31d0f629ee0fd0cf430b31607';
