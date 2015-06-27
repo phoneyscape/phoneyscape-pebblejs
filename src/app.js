7f9c4a912c33b203206f71c4c3b71445a7bbc2bd
Skyler Kidd <f.s.kidd@gmail.com>
2015-06-27 13:04:59 -0700

    Remove Template Code

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@
 
 //import statements
 var UI = require('ui');
-var Vector2 = require('vector2');
+var ajax = require('ajax');
 
 //create main app
 var main = new UI.Card({
@@ -18,47 +18,3 @@ var main = new UI.Card({
 
 //show the app
 main.show();
-
-//action events below this line
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
-  });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
