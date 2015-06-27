e9ef5a8cf5c708c5f0e3a650adb7c89c7f1278f2
Steven Bock <steven.bockster@gmail.com>
2015-06-27 15:03:33 -0700

    Bundling the PebbleJS SDK

---
+++ build/basalt/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Escape",
+  .company = "f.s.kidd@gmail.com",
+  .icon_resource_id = RESOURCE_ID_IMAGE_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x27, 0xDE, 0xC8, 0x20, 0x1F, 0x68, 0x49, 0xF2, 0x9C, 0x76, 0x38, 0x86, 0xAA, 0xF2, 0xD5, 0xEE },
+  .virtual_size = 0xb6b6
+};
